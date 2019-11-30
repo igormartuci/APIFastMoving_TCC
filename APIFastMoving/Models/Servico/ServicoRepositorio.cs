865ad43a2e421eca6401d72bc9143cbb99e7280c
igor <igormartuci99@gmail.com>
2019-11-30 18:12:31 -0300

    Inicialização da Api

---
+++ APIFastMoving/Models/Servico/ServicoRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIFastMoving.Models.Servico
+{
+
+    public class ServicoRepositorio : IRepositorio<Servico>
+    {
+        public void Delete(Servico item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Servico> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Servico GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Servico item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Servico Login(Servico item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Servico item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
